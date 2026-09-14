eabbbbaaa2bb2ca6941a7dbe2c0346a99ad62c17
Steven-Hazad <phanuth.hun@gmail.com>
2026-09-14 22:46:52 +0700

    Order status

---
+++ NTSR.Domain/Enums/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTSR.Domain.Enums;
+
+public enum OrderStatus
+{
+    New,
+    InPreparation,
+    Ready,
+    Completed,
+    Cancelled
+}
