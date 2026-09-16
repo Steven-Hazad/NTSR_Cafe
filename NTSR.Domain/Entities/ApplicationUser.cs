7b911d483c2de60e897a42a04292411289e19637
Steven-Hazad <phanuth.hun@gmail.com>
2026-09-16 22:03:37 +0700

    Tunel and Auth

---
+++ NTSR.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTSR.Domain.Entities;
+
+public class ApplicationUser
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string FullName { get; set; } = string.Empty;
+    public string Role { get; set; } = "Cashier"; // Admin, Manager, Cashier
+    public bool IsActive { get; set; } = true;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public ICollection<Order> Orders { get; set; } = new List<Order>();
+}
---
+++ NTSR.Domain/Entities/OrderItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTSR.Domain.Entities;
+
+public class OrderItem
+{
+    public int Id { get; set; }
+
+    public Guid OrderId { get; set; }
+    public Order Order { get; set; } = null!;
+
+    public int MenuItemId { get; set; }
+    public MenuItem MenuItem { get; set; } = null!;
+
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+    public string? Notes { get; set; }
+}
