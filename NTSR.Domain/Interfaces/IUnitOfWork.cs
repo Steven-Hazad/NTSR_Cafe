b78301599b14e351ab4df784d78ff95819d8b7f5
Steven-Hazad <phanuth.hun@gmail.com>
2026-09-16 22:09:51 +0700

    That completes NTSR.Domain

---
+++ NTSR.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NTSR.Domain.Entities;
+
+namespace NTSR.Domain.Interfaces;
+
+public interface IUnitOfWork : IDisposable
+{
+    IRepository<Order> Orders { get; }
+    IRepository<OrderItem> OrderItems { get; }
+    IRepository<MenuItem> MenuItems { get; }
+    IRepository<Table> Tables { get; }
+    IRepository<ApplicationUser> Users { get; }
+
+    Task<int> SaveChangesAsync();
+}
