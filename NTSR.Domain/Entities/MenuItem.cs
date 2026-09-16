7821de8e6d0de93c74ef8e136d8a822ab629c8f0
Steven-Hazad <phanuth.hun@gmail.com>
2026-09-16 22:01:20 +0700

    MenuItem.cs

---
+++ NTSR.Domain/Entities/MenuItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTSR.Domain.Entities;
+
+public class MenuItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+    public decimal UnitPrice { get; set; }
+    public string? ImageUrl { get; set; }
+    public bool IsAvailable { get; set; } = true;
+
+    public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+}
