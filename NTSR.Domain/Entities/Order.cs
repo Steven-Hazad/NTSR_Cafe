4037e760125f4e07fbc1c5f40e087a311ac9e85b
Steven-Hazad <phanuth.hun@gmail.com>
2026-09-16 22:02:16 +0700

    Prepare for TABLE

---
+++ NTSR.Domain/Entities/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NTSR.Domain.Enums;
+
+namespace NTSR.Domain.Entities;
+
+public class Order
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string OrderNumber { get; set; } = string.Empty;
+    public OrderType OrderType { get; set; }
+
+    public int? TableId { get; set; }
+    public Table? Table { get; set; }
+
+    public Guid? CashierId { get; set; }
+    public ApplicationUser? Cashier { get; set; }
+
+    public decimal SubTotal { get; set; }
+    public decimal TaxAmount { get; set; }
+    public decimal TotalAmount { get; set; }
+
+    public OrderStatus Status { get; set; } = OrderStatus.New;
+    public PaymentStatus PaymentStatus { get; set; } = PaymentStatus.Unpaid;
+    public PaymentMethod? PaymentMethod { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+}
