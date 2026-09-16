5f0cefd51c82f51859654b382f1a08d671c361bf
Steven-Hazad <phanuth.hun@gmail.com>
2026-09-16 22:00:46 +0700

    Table class

---
+++ NTSR.Domain/Entities/Table.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTSR.Domain.Entities;
+
+public class Table
+{
+    public int Id { get; set; }
+    public string TableNumber { get; set; } = string.Empty;
+    public int Capacity { get; set; }
+    public string? QRCodeToken { get; set; }
+    public string Status { get; set; } = "Available"; // Available, Occupied, Reserved  
+
+    public ICollection<Order> Orders { get; set; } = new List<Order>();
+}
