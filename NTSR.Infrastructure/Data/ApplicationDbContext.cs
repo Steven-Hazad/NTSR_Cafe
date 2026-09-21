c432c973a77bc32bfd0759cecc8165050058d5a4
Steven-Hazad <phanuth.hun@gmail.com>
2026-09-21 22:27:37 +0700

    Microsoft.Hosting.Lifetime: Information: Now listening on: https://localhost:7001

---
+++ NTSR.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NTSR.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTSR.Infrastructure.Data;
+    
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<ApplicationUser> Users => Set<ApplicationUser>();
+    public DbSet<Table> Tables => Set<Table>();
+    public DbSet<MenuItem> MenuItems => Set<MenuItem>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
