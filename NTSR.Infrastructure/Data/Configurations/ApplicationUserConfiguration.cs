2e4dad9d7156216edcc202a9f3e404d873d35ce3
Steven-Hazad <phanuth.hun@gmail.com>
2026-09-23 22:46:07 +0700

    ApplicationUserConfiguration

---
+++ NTSR.Infrastructure/Data/Configurations/ApplicationUserConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NTSR.Domain.Entities;
+
+namespace NTSR.Infrastructure.Data.Configurations;
+
+public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.FullName).HasMaxLength(100).IsRequired();
+        builder.Property(u => u.Role).HasMaxLength(20).IsRequired();
+        builder.Property(u => u.IsActive).HasDefaultValue(true);
+        builder.Property(u => u.CreatedAt).HasDefaultValueSql("GETUTCDATE()");
+    }
+}
