d6abc9899552fd1dd09002d787f3b2feb8ac1a93
Steven-Hazad <phanuth.hun@gmail.com>
2026-09-23 22:47:54 +0700

    TableConfiguration

---
+++ NTSR.Infrastructure/Data/Configurations/TableConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NTSR.Domain.Entities;
+
+namespace NTSR.Infrastructure.Data.Configurations;
+
+public class TableConfiguration : IEntityTypeConfiguration<Table>
+{
+    public void Configure(EntityTypeBuilder<Table> builder)
+    {
+        builder.HasKey(t => t.Id);
+        builder.Property(t => t.TableNumber).HasMaxLength(10).IsRequired();
+        builder.HasIndex(t => t.TableNumber).IsUnique();
+        builder.Property(t => t.Capacity).IsRequired();
+        builder.Property(t => t.QRCodeToken).HasMaxLength(100);
+        builder.HasIndex(t => t.QRCodeToken).IsUnique().HasFilter("[QRCodeToken] IS NOT NULL");
+        builder.Property(t => t.Status).HasMaxLength(20).IsRequired();
+    }
+}
