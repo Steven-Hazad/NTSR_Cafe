47a8ebf826516ec39b3fa0a5283ac9412f85c994
Steven-Hazad <phanuth.hun@gmail.com>
2026-09-23 22:53:10 +0700

    oRDER TRABLE

---
+++ NTSR.Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NTSR.Domain.Entities;
+
+namespace NTSR.Infrastructure.Data.Configurations;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasKey(o => o.Id);
+        builder.Property(o => o.OrderNumber).HasMaxLength(20).IsRequired();
+        builder.Property(o => o.OrderType).HasConversion<string>().HasMaxLength(20).IsRequired();
+        builder.Property(o => o.SubTotal).HasColumnType("decimal(18,2)");
+        builder.Property(o => o.TaxAmount).HasColumnType("decimal(18,2)");
+        builder.Property(o => o.TotalAmount).HasColumnType("decimal(18,2)");
+        builder.Property(o => o.Status).HasConversion<string>().HasMaxLength(20).IsRequired();
+        builder.Property(o => o.PaymentStatus).HasConversion<string>().HasMaxLength(20).IsRequired();
+        builder.Property(o => o.PaymentMethod).HasConversion<string>().HasMaxLength(20);
+        builder.Property(o => o.CreatedAt).HasDefaultValueSql("GETUTCDATE()");
+
+        builder.HasOne(o => o.Table)
+            .WithMany(t => t.Orders)
+            .HasForeignKey(o => o.TableId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(o => o.Cashier)
+            .WithMany(u => u.Orders)
+            .HasForeignKey(o => o.CashierId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
---
+++ NTSR.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NTSR.Domain.Entities;
+
+namespace NTSR.Infrastructure.Data.Configurations;
+
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.HasKey(oi => oi.Id);
+        builder.Property(oi => oi.Quantity).IsRequired();
+        builder.Property(oi => oi.UnitPrice).HasColumnType("decimal(18,2)");
+        builder.Property(oi => oi.Notes).HasMaxLength(250);
+
+        builder.HasOne(oi => oi.Order)
+            .WithMany(o => o.OrderItems)
+            .HasForeignKey(oi => oi.OrderId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(oi => oi.MenuItem)
+            .WithMany(m => m.OrderItems)
+            .HasForeignKey(oi => oi.MenuItemId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
