cd1594bf36f8b4a0ce8b8f396ff308db8f905139
Steven-Hazad <phanuth.hun@gmail.com>
2026-09-24 21:34:37 +0700

    Folder structure inside NTSR.Services

---
+++ NTSR.Services/DTOs/MenuItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace NTSR.Services.DTOs;
+
+public class MenuItemDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+    public decimal UnitPrice { get; set; }
+    public string? ImageUrl { get; set; }
+    public bool IsAvailable { get; set; }
+}
